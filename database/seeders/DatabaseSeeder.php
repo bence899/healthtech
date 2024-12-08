71f88136528c6c11b5b8a179ad9a79e0cb9f3e1a
bence899 <bence.luo899@gmail.com>
2024-12-08 21:03:11 +0000

    fix: include PatientSeeder in DatabaseSeeder for data seeding

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -6,6 +6,7 @@ use App\Models\User;
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 use Database\Seeders\DoctorSeeder;
+use Database\Seeders\PatientSeeder;
 
 class DatabaseSeeder extends Seeder
 {
